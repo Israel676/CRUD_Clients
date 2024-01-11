9daa90a8003bf512f205bd7477797118dbb6a655
Israel Rocha <israelrochafigueiredo@gmail.com>
2024-01-11 16:05:58 -0300

    commit

---
+++ Models/ClientController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Clients.Models;
+
+namespace CRUD_Clients.Models
+{
+    public class ClientController
+    {
+        private readonly string connectionString = "";
+    }
+}
